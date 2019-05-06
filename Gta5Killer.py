8aa5e797908476385a67fceec734298f53a27d78
ztaer <ztaerkiller@gmail.com>
2019-05-06 18:57:51 +0800

    构思了检测CEO隐匿冷却时间

--- Gta5Killer.py
+++ Gta5Killer.py
@@ -26,6 +26,7 @@ D = 0x20
 M = 0x32
 R = 0x13
 T = 0x14
+C = 0x2E
 
 O1 = 0x02
 O2 = 0x03
@@ -38,24 +39,14 @@ O8 = 0x09
 O9 = 0x0A
 O0 = 0x0B
 
-NP_2 = 0x50
-NP_4 = 0x4B
-NP_6 = 0x4D
-NP_8 = 0x48
-
 UP = 0xC8
 LEFT = 0xCB
 RIGHT = 0xCD
 DOWN = 0xD0
 
-# mouse constants
-mouse_move = 0x0001
-mouse_lb_press = 0x0002
-mouse_lb_release = 0x0004
-mouse_rb_press = 0x0008
-mouse_rb_release = 0x0010
-mouse_mb_press = 0x0020
-mouse_mb_release = 0x0040
+CAPITAL = 0x3A
+ENT = 0x1C
+
 # 按键仓库-END
 
 # a)获取键盘按键-BGN
@@ -148,7 +139,7 @@ def left_click():
 
 # c)功能调试区域-BGN
 
-# 0. -T- 火箭筒快速发射
+# 0. -G- 火箭筒快速发射
 def t_rpg():
     PressKey(O5)
     time.sleep(0.2)
@@ -170,6 +161,30 @@ def t_rpg():
 
     left_click()
 
+#   1. -K- 秒自杀
+def suicide():
+    PressKey(M)
+    time.sleep(0.2)
+    ReleaseKey(M)
+
+    PressKey(UP)
+    time.sleep(0.2)
+    ReleaseKey(UP)
+
+    PressKey(UP)
+    time.sleep(0.2)
+    ReleaseKey(UP)
+
+    PressKey(C)
+    time.sleep(0.2)
+    PressKey(CAPITAL)
+    time.sleep(0.2)
+    ReleaseKey(CAPITAL)
+    PressKey(ENT)
+    time.sleep(0.2)
+    ReleaseKey(C)
+    ReleaseKey(ENT)
+
 #   2. -R- 快速换巴雷特子弹
 def r_gun_reset():
     PressKey(O5)
@@ -180,22 +195,55 @@ def r_gun_reset():
     time.sleep(0.2)
     ReleaseKey(O9)
 
+door = False
+#   开启CEO隐匿
+#   如果是door为false
+#       否则,memory值传入时间验证器
+#           验证时间(单独书写模块):
+#               获取nowhor/nowmin当前系统小时/分钟
+#               如果小时相等则分钟相减result>8则返回true
+#               如果小时相减等1,则60 - memory的分钟+ 当前分钟数 result> 8是则返回true
+#                   否则响应结果次数报警并且返回false
+#   是: 开启隐匿,door = True,memory存入当前时间
+
+#   获取当前时间
+def cooling_time():
+    localtime = time.localtime(time.time())
+    nowHur = localtime[3]
+    nowMin = localtime[4]
+    return (nowHur,nowMin)
+
+
+
 # c)功能调试区域-END
 
 #   #   功能构思:
+# 用法:
+#   0. -KS- 开启功能
+#   1. -JS- 关闭功能
+#   2. -TC- 退出程序
+
 # 功能:
-#   0. -T- 火箭筒快速发射
+#   0. -G- 火箭筒快速发射
 #   1. -K- 秒自杀
 #   2. -R- 快速换巴雷特子弹
 #   3. -X- 秒吃CEO药品
 #   4. -B- 秒购买护甲
 #   5. -0- 秒购买火箭筒子弹
+#   6. -CEO-
+#   --- 冷却时间不够将不执行,后台计入时间,单位为分钟,差几分钟响应几声报警声 ---
+#   7. -CYN- CEO隐匿   ( 3分钟 - 冷却5分钟 )
+#   8. -LYN- 莱斯特隐匿 ( 1分钟 - 冷却5分钟 )
+#
+#   获取时间 import time
+#   localtime = time.localtime(time.time())
+#   localtime[4] = 分钟数
 
 if __name__ == "__main__":
     print("-----欢迎使用OO7的'GTA5_Killer v1.0'撕逼玩家专用程序( 此程序完全免费只供个人研究学习 )")
     print("-----开源地址/更新地址:\n")
-    print("---用法:\n--开启功能: 进入游戏连续按下'KS'字母键\n--关闭功能: 游戏中连续按下'JS'字母键\n--退出程序: 先'关闭功能后'输入'TC'字母键,即可完全关闭程序\n--提示: \n  ‘开启功能’时会有 1 声提示音\n  ‘关闭功能’时会有 2 声提示音\n  '关闭功能'后输入'KS'依然可以再一次开启功能\n")
-    print("---功能:\n -T- 火箭筒快速发射\n -R- 快速换巴雷特子弹\n -X- 秒吃CEO药品( CEO时使用 )\n -B- 秒购买护甲( CEO时使用 )\n -0- 秒购买火箭筒子弹")
+    print("---用法:\n--开启功能: 进入游戏同时按下'KS'字母键\n--关闭功能: 游戏中同时按下'JS'字母键\n--退出程序: 先'关闭功能后'同时按下'TC'字母键,即可完全关闭程序\n--提示: \n  ‘开启功能’时会有 1 声提示音\n  ‘关闭功能’时会有 2 声提示音\n  '关闭功能'后输入'KS'依然可以再一次开启功能\n")
+    print("---功能:\n -G- 火箭筒快速发射\n -R- 快速换巴雷特子弹\n -X- 秒吃CEO药品( CEO时使用 )\n -B- 秒购买护甲( CEO时使用 )\n -0- 秒购买火箭筒子弹")
 
     while( True ):
         bgn = False
@@ -211,7 +259,7 @@ if __name__ == "__main__":
             continue
         while(bgn):
             keysIng = key_check()
-            if( 'T' in keysIng ):
+            if( 'G' in keysIng ):
                 t_rpg()
                 continue
             elif( 'R' in keysIng ):
